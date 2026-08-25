322a7a19fb5822ba3d9cdbd2872089d17bbfe5ce
tikaramjoshi <tikaramjoshi883@gmail.com>
2026-08-25 23:44:23 +0545

    Passenger  Add Font Awesome icons to footer links

--- passenger/dashboard.php
+++ passenger/dashboard.php
@@ -212,26 +212,39 @@ unset($route);
         <div class="last-main">
             <div class="last-link">
                 <h3>Quick Link</h3>
-                <a href="#">Home</a>
+                <!-- <a href="#">Home</a>
                 <a href="#">Gallery</a>
                 <a href="../policy.php">Policy</a>
                 <a href="profile.php">Profile</a>
-                <a href="booking_history.php">Booking History</a>
+                <a href="booking_history.php">Booking History</a> -->
+                <a href="#"><i class="fa fa-home"></i>&nbsp; Home</a>
+                <a href="#"><i class="fa fa-picture-o"></i>&nbsp; Gallery</a>
+                <a href="../policy.php"><i class="fa fa-file-text"></i>&nbsp; Policy</a>
+                <a href="profile.php"><i class="fa fa-user"></i>&nbsp; Profile</a>
+                <a href="booking_history.php"><i class="fa fa-history"></i>&nbsp; Booking History</a>
             </div>
             <div class="last-contact" id="contactSection">
                 <h3>Contact</h3>
-                <p>Email: <a href="mailto:tikaramj519@gmail.com">tikaramj519@gmail.com</a></p>
+                <!-- <p>Email: <a href="mailto:tikaramj519@gmail.com">tikaramj519@gmail.com</a></p>
                 <p>Phone: <a href="tel:+9779840792553">+9779840792553</a></p>
-                <p>Whatsapp: <a href="https://wa.me/9779840792553" target="_blank">+9779840792553</a></p>
+                <p>Whatsapp: <a href="https://wa.me/9779840792553" target="_blank">+9779840792553</a></p> -->
+                <p>Email: <a href="mailto:tikaramj519@gmail.com"><i class="fa fa-envelope"></i>&nbsp; tikaramj519@gmail.com</a></p>
+                <p>Phone: <a href="tel:+9779840792553"><i class="fa fa-phone"></i>&nbsp; +9779840792553</a></p>
+                <p>Whatsapp: <a href="https://wa.me/9779840792553" target="_blank"><i class="fa fa-whatsapp"></i>&nbsp; +9779840792553</a></p>
             </div>
             <div class="last-about" id="aboutSection">
                 <h3>About</h3>
                 <ul>
-                    <li>Online Bus Ticket Booking System</li>
+                    <!-- <li>Online Bus Ticket Booking System</li>
                     <li>Easy Bus Search</li>
                     <li>24/7 Customer Support</li>
                     <li>Safe Online Booking</li>
-                    <li>No Cancel Ticket</li>
+                    <li>No Cancel Ticket</li> -->
+                    <li><i class="fa fa-bus"></i>&nbsp; Online Bus Ticket Booking System</li>
+                    <li><i class="fa fa-search"></i>&nbsp; Easy Bus Search</li>
+                    <li><i class="fa fa-headphones"></i>&nbsp; 24/7 Customer Support</li>
+                    <li><i class="fa fa-shield"></i>&nbsp; Safe Online Booking</li>
+                    <li><i class="fa fa-ban"></i>&nbsp; No Cancel Ticket</li>
                 </ul>
             </div>
         </div>
